0820c3f32beabfbfe24a6c0ea2dfed145e5b1648
silva3265 <silva3265@gmail.com>
2024-03-13 15:18:13 -0300

    Boas práticas: crie cópias defensivas

---
+++ src/agenda/Agendamento.java
@@ -0,0 +1,29 @@
+package agenda;
+
+public class Agendamento {
+
+	private final Horario horario;
+	private String descricao;
+
+	public Agendamento(Horario horario, String descricao) {
+		this.horario = new Horario(horario.getHora(), horario.getMinuto());
+		this.descricao = descricao;
+	}
+
+	public Horario getHorario() {
+		return new Horario(horario.getHora(), horario.getMinuto());
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getHorarioFormatado() {
+		return horario.formatar();
+	}
+
+}
